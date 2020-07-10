708e737f983fa70e9175ebb1158c782dd0885779
hbyyy <qjaduddl94@gmail.com>
2020-07-10 17:14:47 +0900

    결제모듈 테스트

---
+++ app/product/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+# Create your views here.
+from django.views.decorators.http import require_POST
+
+from product.models import Product
+
+
+def index(request):
+    product = Product.objects.first()
+    return render(request, 'index.html', {'product': product})
+
+
+@login_required
+@require_POST
+def order_success_view(request):
+    imp_uid = request.POST.get('imp_uid')
+    print(request.POST)
+    print(request)
+    return render(request, 'payment_success.html', {'imp_uid', imp_uid})
